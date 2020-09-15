397d46ad134af3c890ea414e7dac8b8a8e891bf6
Geiger <geiga@shopbop.com>
2020-09-15 15:28:29 -0500

    finished 3.2.5

--- assets/js/game.js
+++ assets/js/game.js
@@ -17,6 +17,12 @@ console.log(enemyNames);
 console.log(enemyNames[0]);
 console.log(enemyNames[1]);
 console.log(enemyNames[2]);
+console.log(enemyNames.length);
+for(var i = 0; i < enemyNames.length; i++) {
+    console.log(enemyNames[i]);
+    console.log(i);
+    console.log(enemyNames[i] + " is at " + i + " index");
+}
 var enemyHealth = 50;
 var enemyAttack = 12;
 
