aed3dd500314b98785383a8a7aa14cdce446ec6c
geigea84 <geigea84@gmail.com>
2020-09-15 19:14:09 -0500

    finished 3.2.6

--- assets/js/game.js
+++ assets/js/game.js
@@ -26,7 +26,7 @@ for(var i = 0; i < enemyNames.length; i++) {
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function () {
+var fight = function (enemyName) {
     window.alert("Welcome to Robot Gladiators!");
 
 var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter FIGHT or SKIP to choose");
@@ -90,4 +90,6 @@ else {
 
 };
 
-//fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
